c0c6510788d94262754ddaea2e94d09c301cba12
brendanerquiaga <brendanerquiaga@gmail.com>
2019-08-05 13:56:42 -0700

    Working example of hitting icon-api & returning a list

--- src/lambdas/saveIcon.js
+++ src/lambdas/saveIcon.js
@@ -1,31 +1,41 @@
 const httpResponse = require('../utils/httpResponse.js');
 const constants = require('../utils/constants.js');
-const axios = require('axios');
+const requestPromise = require('request-promise-native');
 
 
 exports.getIconFromAPI = async (event, context) => {
-  var nf_token_url = "https://www.iconfinder.com/api/v3/oauth2/token";
-
   let requestURL = constants.ICON_API_U;
   requestURL += '?query=' + 'cat';
   requestURL += '&count=' + 5;
+  requestURL += '&vector=' + 'true';
+  // requestURL += '&style=' + 'Flat';
   requestURL += '&client_id=' + constants.ICON_API_I;
   requestURL += '&client_secret=' + constants.ICON_API_S;
 
   let iconAPIResponse = await IconAPIResponse(requestURL);
-  
-  return httpResponse(200, iconAPIResponse);
+
+  return httpResponse(200, iconAPIResponse, true);
 };
 
-async function IconAPIResponse(url) {
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-      .then(response => {
-        console.log(JSON.stringify(response));
-        resolve(response);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
+async function IconAPIResponse(queryParams) {
+  let options = {
+    method: 'GET',
+    uri: 'https://' + constants.ICON_API_H + constants.ICON_API_U,
+    qs: {
+      client_id: constants.ICON_API_I,
+      client_secret: constants.ICON_API_S,
+      query: 'cat',
+      count: 5,
+      vector: 'true'
+    },
+    json:true
+  }
+
+  return requestPromise(options)
+    .then(function(response) {
+      return response;
+    })
+    .catch(function(error){
+      console.log("ERR: " + error);
+    });
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 module.exports = Object.freeze({
   ICON_FOLDER_PATH: "icon-api-bucket",
   ICON_NAME_ENDING_CHAR: '.',
-  ICON_API_U: 'https://api.iconfinder.com/v3/icons/search',
+  ICON_API_H: 'api.iconfinder.com',
+  ICON_API_U: '/v3/icons/search',
   ICON_API_I: 'AiH7tb9Y61MC21QdlJdTinkay9F4M8QBdEdIJr0aI7waDwyo9igrM06NWRUqu6Cp',
   ICON_API_S: 'aCLtkY8dGM0sap4vYjP43cuxsQ7evHDi5nC0VJrAiIaYmubkg35Rv2zQb7VsR6m2'
 });
