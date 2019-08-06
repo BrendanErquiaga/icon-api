053b0babc2e5f8e50f63b444b02ee055690f4b7c
Brendan <brendanerquiaga@gmail.com>
2019-08-05 22:16:34 -0700

    Properly fetching a list of icon strings

--- src/lambdas/saveIcon.js
+++ src/lambdas/saveIcon.js
@@ -4,38 +4,61 @@ const requestPromise = require('request-promise-native');
 
 
 exports.getIconFromAPI = async (event, context) => {
-  let requestURL = constants.ICON_API_U;
-  requestURL += '?query=' + 'cat';
-  requestURL += '&count=' + 5;
-  requestURL += '&vector=' + 'true';
-  // requestURL += '&style=' + 'Flat';
-  requestURL += '&client_id=' + constants.ICON_API_I;
-  requestURL += '&client_secret=' + constants.ICON_API_S;
+  let searchTerm = event.queryStringParameters.search || 'cat';
+  let getVectorIconsOnly = event.queryStringParameters.vector || 'false';
 
-  let iconAPIResponse = await IconAPIResponse(requestURL);
+  if (getVectorIconsOnly != 'true' || getVectorIconsOnly != 'false') {
+    getVectorIconsOnly = 'false';
+  }
 
-  return httpResponse(200, iconAPIResponse, true);
+  let iconAPIResponse = await IconAPIResponse(searchTerm, getVectorIconsOnly);
+  let filteredAPIResponse = filterAPIResponse(iconAPIResponse);
+
+  return httpResponse(200, filteredAPIResponse, true);
 };
 
-async function IconAPIResponse(queryParams) {
+function filterAPIResponse(apiResponse) {
+  let iconPreviewURLs = [];
+
+  apiResponse.icons.forEach(function (icon) {
+    iconPreviewURLs.push(getIconOfDesiredSize(icon));
+  });
+
+  return iconPreviewURLs;
+}
+
+function getIconOfDesiredSize(icon, iconSize) {
+  let iconURL = "",
+      desiredIconSize = iconSize || constants.DESIRED_ICON_SIZE;
+  icon.raster_sizes.forEach(function (rasterSize) {
+    if (rasterSize.size === desiredIconSize) {
+      iconURL = rasterSize.formats[0].preview_url;
+    }
+  });
+
+  return iconURL;
+}
+
+async function IconAPIResponse(searchTerm, getVectorIconsOnly) {
   let options = {
     method: 'GET',
     uri: 'https://' + constants.ICON_API_H + constants.ICON_API_U,
     qs: {
       client_id: constants.ICON_API_I,
       client_secret: constants.ICON_API_S,
-      query: 'cat',
-      count: 5,
-      vector: 'true'
+      count: constants.ICON_API_ICON_COUNT,
+      premium: 0,
+      query: searchTerm,
+      vector: getVectorIconsOnly
     },
-    json:true
+    json: true
   }
 
   return requestPromise(options)
-    .then(function(response) {
+    .then(function (response) {
       return response;
     })
-    .catch(function(error){
+    .catch(function (error) {
       console.log("ERR: " + error);
     });
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 module.exports = Object.freeze({
   ICON_FOLDER_PATH: "icon-api-bucket",
   ICON_NAME_ENDING_CHAR: '.',
+  ICON_API_ICON_COUNT: 5,
+  DESIRED_ICON_SIZE: 64,
   ICON_API_H: 'api.iconfinder.com',
   ICON_API_U: '/v3/icons/search',
   ICON_API_I: 'AiH7tb9Y61MC21QdlJdTinkay9F4M8QBdEdIJr0aI7waDwyo9igrM06NWRUqu6Cp',
